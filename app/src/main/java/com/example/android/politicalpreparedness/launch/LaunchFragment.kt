13d4f1c6a515bc46268cd66e80e0b8bbf42a5568
rajesh <rajesh.k.khadka@gmail.com>
2020-05-15 21:52:20 +0545

    add navigation

--- app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -23,11 +23,11 @@ class LaunchFragment : Fragment() {
     }
 
     private fun navToElections() {
-//        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
     }
 
     private fun navToRepresentatives() {
-//        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
     }
 
 }
